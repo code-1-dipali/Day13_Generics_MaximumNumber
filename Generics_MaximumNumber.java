e08f022a0b793593ae30bc51b857e466b8476087
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-19 15:50:19 +0530

    MaxStringNumAmongThreeNum

--- Generics_MaximumNumber.java
+++ Generics_MaximumNumber.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class TestMaximum  {
 	Scanner sc = new Scanner(System.in);
 	
-	public Double getMaxNumber(Double num1, Double num2, Double num3) {
-		Double max = num1;
-		if(num2.compareTo(max) > 0) 
+	public String getMaxStr(String num1, String num2, String num3) {
+		String max = num1;
+		if(max.compareTo(num2) > max.length()) 
 			max = num2;
-		else if(num3.compareTo(max) > 0)
+		if(max.compareTo(num3) > max.length())
 			max = num3;
-		
-		 return max;
+		System.out.println("====>"+max);
+		return max;
 	}
 }

5948e96b486a819fe771f6e9ff44040fc4fbca7f
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-19 15:56:51 +0530

    TakeMoreThanThreeGenericParameters

--- Generics_MaximumNumber.java
+++ Generics_MaximumNumber.java
@@ -1,16 +1,22 @@
 package com.bl.program;
-import java.util.Scanner;
 
-public class TestMaximum  {
-	Scanner sc = new Scanner(System.in);
+public class TestMaximum<t extends Comparable<t>> {
 	
-	public String getMaxStr(String num1, String num2, String num3) {
-		String max = num1;
-		if(max.compareTo(num2) > max.length()) 
-			max = num2;
-		if(max.compareTo(num3) > max.length())
-			max = num3;
-		System.out.println("====>"+max);
-		return max;
+	public static <t extends Comparable<t>>t getMaximumNumber(t...num ) {
+		  t max = num[0];
+		  String t = "";
+		  if(t.getClass() != max.getClass()) {
+			  for(t element : num) {
+				  if(element.compareTo(max) > 0 ) 
+					  max = element;
+			  }
+		  }
+		  else {
+			  for(t element : num) {
+				  if(element.compareTo(max) > ((String) max).length() ) 
+					  max = element;
+			  }
+		  }
+		  return max;
 	}
 }

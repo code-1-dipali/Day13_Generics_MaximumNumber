b79d99a3f6f0946f90fa9c8c657ea544c7f85d40
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-19 15:27:41 +0530

    _MaxIntAmongThreeNum

--- Generics_MaximumNumber.java
+++ Generics_MaximumNumber.java
@@ -1,8 +1,17 @@
 package com.bl.program;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+public class TestMaximum  {
+	Scanner sc = new Scanner(System.in);
+	
+	public Integer getMaxNumber(Integer num1, Integer num2, Integer num3) {
+		Integer max = num1;
+		if(num2.compareTo(max) > 0) 
+			max = num2;
+		else if(num3.compareTo(max) > 0)
+			max = num3;
+		
+		 return max;
+	}
 }
